d5478d875b4a92566b5553453b0c6428e53a1b3d
OragonEfreet <kevin.dorange@gmail.com>
2015-03-13 20:07:40 +0100

    added dummy code

--- print.py
+++ print.py
@@ -1,1 +1,10 @@
+class ClassName(object):
+  """docstring for ClassName"""
+  def __init__(self, arg):
+    super(ClassName, self).__init__()
+    self.arg = arg
+    
+
+def tititatatoto
+
 print("coucou")
